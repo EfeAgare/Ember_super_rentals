8ee4eaecf903dc51c618619371564d2c987cb764
Efe <knowledgeagare157@gmail.com>
2020-02-02 21:20:36 +0100

    add navbar component

--- tests/integration/components/hero-test.js
+++ tests/integration/components/hero-test.js
@@ -6,21 +6,14 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | hero', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders the content inside a hero image', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Hero />`);
+    await render(hbs`<Hero> Hello World </Hero>`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Hero>
-        template block text
-      </Hero>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('Hello World');
+    assert.dom('.jumbo .hero-text').exists();
   });
 });
