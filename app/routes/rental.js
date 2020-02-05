6ac97d6ea335afcf575a0c1be4d131085b158413
Efe <knowledgeagare157@gmail.com>
2020-02-05 22:58:09 +0100

    ember data model and loading the models in the routes

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,21 +1,31 @@
 import Route from "@ember/routing/route";
-
+import { inject as service } from '@ember/service';
 export default class RentalRoute extends Route {
+  // async model(params) {
+
+        // using routes
+  //   const COMMUNITY_CATEGORIES = ["Condo", "Townhouse", "Apartment"];
+
+  //   let response = await fetch(`/api/rentals/${params.rental_id}.json`);
+
+  //   let { data } = await response.json();
+  //   let { id, attributes } = data;
+  //   let type;
+
+  //   if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //     type = "Community";
+  //   } else {
+  //     type = "Standalone";
+  //   }
+
+  //   return { id, type, ...attributes };
+  // }
+
+
+  // using model
+  @service store;
+
   async model(params) {
-    const COMMUNITY_CATEGORIES = ["Condo", "Townhouse", "Apartment"];
-
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-
-    let { data } = await response.json();
-    let { id, attributes } = data;
-    let type;
-
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = "Community";
-    } else {
-      type = "Standalone";
-    }
-
-    return { id, type, ...attributes };
+    return this.store.find('rental', params.rental_id);
   }
 }
